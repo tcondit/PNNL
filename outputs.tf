3f957d7534ec0eaf4b2150e82a2e384c56c4f4ac
Tim Condit <tcondit@gmail.com>
2016-12-06 16:14:55 -0800

    output two IPs with labels

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,7 @@
-output "ip" {
+output "jenkins_master_ip" {
     value = "${aws_instance.jenkins_master.public_ip}"
+}
+
+output "build_server_ip" {
     value = "${aws_instance.build_server.public_ip}"
 }
